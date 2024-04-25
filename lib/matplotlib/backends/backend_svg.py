c8d1f563261de6b2a897ea5d068c377d7d3dcb91
James Addison <jay@jp-hosting.net>
2024-04-25 11:34:00 +0100

    docs: declare that the identifier scheme is intended to generate repeatable/deterministic results.

--- lib/matplotlib/backends/backend_svg.py
+++ lib/matplotlib/backends/backend_svg.py
@@ -328,8 +328,8 @@ class RendererSVG(RendererBase):
 
     def _get_clippath_id(self, clippath):
         """
-        Returns a stable, unique identifier for the clippath argument object
-        within the current rendering context.
+        Returns a stable, unique, and deterministic identifier for the
+        clippath argument object within the current rendering context.
         """
         if clippath not in self._clip_path_ids:
             self._clip_path_ids[clippath] = len(self._clip_path_ids)
