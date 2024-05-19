78e28a1f9ca8f0808d4aee90d814debc9679e44d
Pranav <kingprawn5000@gmail.com>
2024-05-19 12:30:21 +0530

    Made a more detailed gallery example

--- galleries/examples/statistics/histogram_multihist.py
+++ galleries/examples/statistics/histogram_multihist.py
@@ -15,7 +15,7 @@ shape of a histogram. The Astropy docs have a great section on how to
 select these parameters:
 http://docs.astropy.org/en/stable/visualization/histogram.html
 """
-
+# %%
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -27,25 +27,72 @@ x = np.random.randn(1000, 3)
 fig, ((ax0, ax1), (ax2, ax3)) = plt.subplots(nrows=2, ncols=2)
 
 colors = ['red', 'tan', 'lime']
-ax0.hist(x, n_bins, density=True, histtype='bar', color=colors,
-         label=colors, hatch=['o', '*', '.'],
-         edgecolor=['black', 'red', 'blue'], linewidth=[1, 2, 3])
+ax0.hist(x, n_bins, density=True, histtype="bar", color=colors, label=colors)
 ax0.legend(prop={'size': 10})
 ax0.set_title('bars with legend')
 
-ax1.hist(
-    x, n_bins, density=True, histtype="bar", stacked=True,
-        edgecolor=["black", "yellow", "blue"])
+ax1.hist(x, n_bins, density=True, histtype="bar", stacked=True)
 ax1.set_title('stacked bar')
 
-ax2.hist(x, n_bins, histtype='step', stacked=True, fill=False,
-         linewidth=[1, 2, 3])
+ax2.hist(x, n_bins, histtype="step", stacked=True, fill=False)
 ax2.set_title('stack step (unfilled)')
 
 # Make a multiple-histogram of data-sets with different length.
 x_multi = [np.random.randn(n) for n in [10000, 5000, 2000]]
-ax3.hist(x_multi, n_bins, histtype='bar', hatch=['\\', '/', '-'])
+ax3.hist(x_multi, n_bins, histtype="bar")
 ax3.set_title('different sample sizes')
+
+fig.tight_layout()
+plt.show()
+
+# %%
+# Setting properties for each sample set
+# ------------
+#
+# Plotting a bar chart with sample sets differentiated using:
+# * edgecolors
+# * hatches
+# * linewidths
+# * linestyles
+#
+# Also, these parameters can be specified for all types of
+# histograms (stacked, step, etc.) and not just for the *bar*
+# type histogram as shown in the example.
+
+hatches = ["-", ".", "x"]
+linewidths = [1, 2, 3]
+edgecolors = ["green", "red", "blue"]
+linestyles = ["-", ":", "--"]
+
+fig, ((ax0, ax1), (ax2, ax3)) = plt.subplots(nrows=2, ncols=2)
+
+ax0.hist(
+    x, n_bins, density=True, fill=False, histtype="bar",
+    edgecolor=edgecolors, label=edgecolors
+)
+ax0.legend(prop={"size": 10})
+ax0.set_title("Bars with Edgecolors")
+
+ax1.hist(
+    x, n_bins, density=True, histtype="bar",
+    hatch=hatches, label=hatches
+)
+ax1.legend(prop={"size": 10})
+ax1.set_title("Bars with Hatches")
+
+ax2.hist(
+    x, n_bins, density=True, fill=False, histtype="bar",
+    linewidth=linewidths, edgecolor=edgecolors, label=linewidths
+)
+ax2.legend(prop={"size": 10})
+ax2.set_title("Bars with Linewidths")
+
+ax3.hist(
+    x, n_bins, density=True, fill=False, histtype="bar",
+    linestyle=linestyles, edgecolor=edgecolors, label=linestyles
+)
+ax3.legend(prop={"size": 10})
+ax3.set_title("Bars with Linestyles")
 
 fig.tight_layout()
 plt.show()
